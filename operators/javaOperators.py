ff4460a9a5be6f671cc1507e8cf9f5790da98b80
Yeganeh-MB <yegane.bahoo@gmail.com>
2014-11-16 22:09:35 -0600

    Update javaOperators.py
    ROR eperation

--- operators/javaOperators.py
+++ operators/javaOperators.py
@@ -78,3 +78,46 @@ def  AOR(filename, operators):
             mutantsCount += 1
 
     print('AOR mutation finished. Mutants generated: {0}'.format(mutantsCount))
+    
+    
+    
+  def ROR(filename, operators):
+    """
+    Relatinal Replacement mutation operator 
+    Changes < to > and vice versa
+    And changes =< to >= and vice versa
+    """
+
+    mutantsCount = 0
+
+    if '<' in operators.keys() or '>' in operators.keys() or '>=' in operators.keys() or '=<' in operators.keys():
+        with open(filename + '.java', 'r') as infile:
+            content = infile.read()
+
+    if '>' in operators.keys():
+        for position in operators['>']:
+            with open('ROR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '<' + content[position + 1:] )
+            mutantsCount += 1
+
+    if '<' in operators.keys():
+        for position in operators['<']:
+            with open('ROR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '>' + content[position + 1:] )
+            mutantsCount += 1
+
+    if '=<' in operators.keys():
+        for position in operators['=<']:
+            with open('ROR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '>=' + content[position + 1:] )
+            mutantsCount += 1
+
+    if '>=' in operators.keys():
+        for position in operators['>=']:
+            with open('ROR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '=<' + content[position + 1:] )
+            mutantsCount += 1
+
+    print('ROR mutation finished. Mutants generated: {0}'.format(mutantsCount))
+
+
