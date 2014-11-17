5c42a82925b1c140ab065225be61a65776e90c4d
Yeganeh-MB <yegane.bahoo@gmail.com>
2014-11-16 21:22:36 -0600

    Update javaOperators.py
    Adding AOR operators

--- operators/javaOperators.py
+++ operators/javaOperators.py
@@ -8,7 +8,7 @@ implemented:
   LCR - logical connector replacement
   ABS - absolute value insertion
   UOI - unary operator insertion
-  AOR - ariphmetic operator replacement
+  AOR - arithmetic operator replacement
   ROR - relatinal operator replacement
 """
 
@@ -37,3 +37,44 @@ def  LCR(filename, operators):
             mutantsCount += 1
 
     print('LCR mutation finished. Mutants generated: {0}'.format(mutantsCount))
+    
+    
+    
+def  AOR(filename, operators):
+    """
+    Orarithmetic Replacement mutation operator 
+    Changes + to - and vice versa
+    And changes * to / and vice versa
+    """
+
+    mutantsCount = 0
+
+    if '+' in operators.keys() or '-' in operators.keys() or '/' in operators.keys() or '*' in operators.keys():
+        with open(filename + '.java', 'r') as infile:
+            content = infile.read()
+
+    if '+' in operators.keys():
+        for position in operators['+']:
+            with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '-' + content[position + 1:] )
+            mutantsCount += 1
+
+    if '-' in operators.keys():
+        for position in operators['-']:
+            with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '+' + content[position + 1:] )
+            mutantsCount += 1
+
+    if '/' in operators.keys():
+        for position in operators['/']:
+            with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '*' + content[position + 1:] )
+            mutantsCount += 1
+
+    if '*' in operators.keys():
+        for position in operators['/']:
+           with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '/' + content[position + 1:] )
+            mutantsCount += 1
+
+    print('AOR mutation finished. Mutants generated: {0}'.format(mutantsCount))
