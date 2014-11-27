0e410bc4e938f44ce4a92e9981a06b2790959f72
Yeganeh-MB <yegane.bahoo@gmail.com>
2014-11-26 18:46:03 -0600

    Update javaOperators.py

--- operators/javaOperators.py
+++ operators/javaOperators.py
@@ -39,7 +39,6 @@ def  LCR(filename, operators):
     print('LCR mutation finished. Mutants generated: {0}'.format(mutantsCount))
     
     
-    
 def  AOR(filename, operators):
     """
     Orarithmetic Replacement mutation operator 
@@ -52,12 +51,8 @@ def  AOR(filename, operators):
     if '+' in operators.keys() or '-' in operators.keys() or '/' in operators.keys() or '*' in operators.keys():
         with open(filename + '.java', 'r') as infile:
             content = infile.read()
-
-    if '+' in operators.keys():
-        for position in operators['+']:
-            with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
-                outfile.write( content[:position - 1] + '-' + content[position + 1:] )
-            mutantsCount += 1
+            
+    
 
     if '-' in operators.keys():
         for position in operators['-']:
@@ -65,23 +60,30 @@ def  AOR(filename, operators):
                 outfile.write( content[:position - 1] + '+' + content[position + 1:] )
             mutantsCount += 1
 
+    if '+' in operators.keys():
+        for position in operators['+']:
+            with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '-' + content[position:] )
+            mutantsCount += 1
+            
+    if '*' in operators.keys():
+        for position in operators['*']:
+            with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
+                outfile.write( content[:position - 1] + '/' + content[position:] )
+            mutantsCount += 1
+
     if '/' in operators.keys():
         for position in operators['/']:
             with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
-                outfile.write( content[:position - 1] + '*' + content[position + 1:] )
-            mutantsCount += 1
+                outfile.write( content[:position - 1] + '*' + content[position:] )
+            mutantsCount += 1        
 
-    if '*' in operators.keys():
-        for position in operators['/']:
-           with open('AOR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
-                outfile.write( content[:position - 1] + '/' + content[position + 1:] )
-            mutantsCount += 1
+    
 
     print('AOR mutation finished. Mutants generated: {0}'.format(mutantsCount))
     
     
-    
-  def ROR(filename, operators):
+def  ROR(filename, operators):   
     """
     Relatinal Replacement mutation operator 
     Changes < to > and vice versa
@@ -97,13 +99,13 @@ def  AOR(filename, operators):
     if '>' in operators.keys():
         for position in operators['>']:
             with open('ROR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
-                outfile.write( content[:position - 1] + '<' + content[position + 1:] )
+                outfile.write( content[:position - 1] + '<' + content[position :] )
             mutantsCount += 1
 
     if '<' in operators.keys():
         for position in operators['<']:
             with open('ROR_mutant_{0}_{1}.java'.format(mutantsCount+1, filename), 'w') as outfile:
-                outfile.write( content[:position - 1] + '>' + content[position + 1:] )
+                outfile.write( content[:position - 1] + '>' + content[position :] )
             mutantsCount += 1
 
     if '=<' in operators.keys():
